1e497f1b4515f517cea737ecad898472cf802625
angahpadu <81055755+angahpadu@users.noreply.github.com>
2021-10-24 17:17:41 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,6 +17,35 @@ echo '                       Tunggu 5 Saat!                       '
 echo ' .......................................................... '
 sleep 5
 clear
+if [ "${EUID}" -ne 0 ]; then
+		echo "You need to run this script as root"
+		exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+		echo "OpenVZ is not supported"
+		exit 1
+fi
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://raw.githubusercontent.com/angahpadu/ip/main/access | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Tahniah! Anda Dibenarkan menggunakan SAMVPN...${NC}"
+sleep 5
+else
+clear
+echo ""
+rm -f setup.sh
+echo '                            ...                           '
+echo '        Anda Tidak Dibenarkan Menggunakan Script ini!     '
+echo '                            ...                           '
+echo '    Hubungi Saya di Telegram Untuk Mendapatkan Kebenaran!     '
+echo '                        t.me/sam_sfx     '
+sleep 20
+exit 0
+fi
 echo '============================================='
 echo '          Sila Tunggu sebentar '
 echo 'Process Update & Upgrade Sedang Dijalankan '
